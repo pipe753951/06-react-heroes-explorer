2035e35051788f78e6bd3fbb5579a67275858a42
David Puentes <pipe01112009@gmail.com>
2026-04-04 12:55:43 -0500

    Setting Axios hero API instance.

---
+++ src/heroes/api/hero.api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env["VITE_API_URL"];
+
+const heroApi = axios.create({
+  baseURL: `${BASE_URL}/api/heroes`,
+});
+
+export default heroApi;
