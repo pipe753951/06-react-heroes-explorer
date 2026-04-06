77ca3d4e3dba9a4a3df61f2b7368d3c322e65429
David Puentes <pipe01112009@gmail.com>
2026-04-06 17:54:29 -0500

    Finishing lesson #203 task assignment.

---
+++ src/heroes/actions/getCharacter.action.ts
@@ -0,0 +1,13 @@
+import heroApi, { HERO_API_BASE_URL } from "../api/hero.api";
+import type { Character as Character } from "../types/character.response";
+
+const getCharacter = async (idOrSlug: number | string): Promise<Character> => {
+  const { data: character } = await heroApi.get<Character>("/" + idOrSlug);
+
+  return {
+    ...character,
+    image: `${HERO_API_BASE_URL}/images/${character.image}`,
+  };
+};
+
+export default getCharacter;
