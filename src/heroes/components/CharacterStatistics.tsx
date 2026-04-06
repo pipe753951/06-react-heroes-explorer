16276565ea720532c4b2d581cdd14aedfea21e22
David Puentes <pipe01112009@gmail.com>
2026-04-06 15:13:20 -0500

    Finishing class #200 theory.

--- src/heroes/components/CharacterStatistics.tsx
+++ src/heroes/components/CharacterStatistics.tsx
@@ -1,22 +1,35 @@
+import { queryOptions, useQuery } from "@tanstack/react-query";
+
 import { Users, Heart, Zap, Trophy } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 
 import CharacterStatisticCard from "./CharacterStatisticCard";
+import { getSummary } from "../actions/getSummary.action";
 
 const CharacterStatistics = function () {
+  const { data: summary } = useQuery(
+    queryOptions({
+      queryKey: ["summary-information"],
+      queryFn: getSummary,
+      staleTime: 300000, // 1000ms * 60s * 5m
+    }),
+  );
+
+  console.log(summary);
+
   return (
     <div className="mb-8 grid grid-cols-2 gap-4 md:grid-cols-4">
       <CharacterStatisticCard
         title="Total de personajes"
         icon={<Users className="text-muted-foreground size-5" />}
       >
-        <div className="text-3xl font-bold">16</div>
+        <div className="text-3xl font-bold">{summary?.totalCharacters}</div>
         <div className="mt-2 flex gap-1">
           <Badge variant="secondary" className="text-xs">
-            12 Heroes
+            {summary?.heroCount} Heroes
           </Badge>
           <Badge variant="destructive" className="text-xs">
-            2 Villains
+            {summary?.villainCount} Villains
           </Badge>
         </div>
       </CharacterStatisticCard>
@@ -25,6 +38,7 @@ const CharacterStatistics = function () {
         title="Favoritos"
         icon={<Heart className="text-muted-foreground size-5" />}
       >
+        {/* TODO: obtener y calcular porcentaje favoritos contra total de personajes. */}
         <div className="text-3xl font-bold text-red-600">3</div>
         <p className="text-muted-foreground">18.8% of total</p>
       </CharacterStatisticCard>
@@ -33,16 +47,20 @@ const CharacterStatistics = function () {
         title="El más fuerte"
         icon={<Zap className="text-muted-foreground size-5" />}
       >
-        <div className="text-2xl font-bold">Superman</div>
-        <p className="text-muted-foreground text-xs">Fuerza: 10/10</p>
+        <div className="text-2xl font-bold">{summary?.strongestHero.alias}</div>
+        <p className="text-muted-foreground text-xs">
+          Fuerza: {summary?.strongestHero.strength}/10
+        </p>
       </CharacterStatisticCard>
 
       <CharacterStatisticCard
         title="El más inteligente"
         icon={<Trophy className="text-muted-foreground size-5" />}
       >
-        <div className="text-2xl font-bold">Iron Man</div>
-        <p className="text-muted-foreground text-xs">Inteligencia: 10/10</p>
+        <div className="text-2xl font-bold">{summary?.smartestHero.alias}</div>
+        <p className="text-muted-foreground text-xs">
+          Inteligencia: {summary?.smartestHero.intelligence}/10
+        </p>
       </CharacterStatisticCard>
     </div>
   );
