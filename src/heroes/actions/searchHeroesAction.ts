822af7585ae5fa727b900c7c229ff478de6a3495
David Puentes <pipe01112009@gmail.com>
2026-04-07 18:25:37 -0500

    Finishing lesson #213 task assignment.

---
+++ src/heroes/actions/searchHeroesAction.ts
@@ -0,0 +1,12 @@
+interface Options {
+  name: string;
+  team: string;
+  category: string;
+  universe: string;
+  status: string;
+  strength: string;
+}
+
+const searchCharactersAction = () => {};
+
+export default searchCharactersAction;
