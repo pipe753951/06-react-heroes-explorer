acc9006084ee8e3b7601daca3a08779eec66b02f
David Puentes <pipe01112009@gmail.com>
2026-04-22 21:27:17 -0500

    Finishing lesson #241.

--- src/components/custom/CustomPagination.test.tsx
+++ src/components/custom/CustomPagination.test.tsx
@@ -1,8 +1,30 @@
 import { describe, expect, test, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
-import type { PropsWithChildren } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useState, type PropsWithChildren } from "react";
 
 import CustomPagination from "./CustomPagination";
+
+interface renderCustomPaginationWithTestingHookProps {
+  totalPages: number;
+  defaultPage: number;
+}
+
+const CustomPaginationWithTestingHook = ({
+  totalPages,
+  defaultPage,
+}: renderCustomPaginationWithTestingHookProps) => {
+  const [currentPage, setCurrentPage] = useState(defaultPage);
+
+  const handlePaginationUpdatePage = setCurrentPage;
+
+  return (
+    <CustomPagination
+      totalPages={totalPages}
+      currentPage={currentPage}
+      onUpdatePage={handlePaginationUpdatePage}
+    />
+  );
+};
 
 vi.mock("@/components/ui/button", () => ({
   Button: ({ children, ...props }: PropsWithChildren) => (
@@ -32,5 +54,28 @@ describe("CustomPagination", () => {
     const nextButton = screen.getByText("Siguiente");
 
     expect(nextButton.getAttributeNames()).toContain("disabled");
+  });
+
+  test("must have page #3 button selected when expected page is selected.", () => {
+    render(<CustomPaginationWithTestingHook totalPages={5} defaultPage={3} />);
+
+    const buttonThree = screen.getByText("3");
+
+    expect(buttonThree.getAttribute("variant")).toBe("default");
+  });
+
+  test("must disable page #3 button when page #2 is pressed while page #3 is selected", () => {
+    render(<CustomPaginationWithTestingHook totalPages={5} defaultPage={3} />);
+
+    const buttonTwo = screen.getByText("2");
+    const buttonThree = screen.getByText("3");
+
+    expect(buttonTwo.getAttribute("variant")).toBe("outline");
+    expect(buttonThree.getAttribute("variant")).toBe("default");
+
+    fireEvent.click(buttonTwo);
+
+    expect(buttonTwo.getAttribute("variant")).toBe("default");
+    expect(buttonThree.getAttribute("variant")).toBe("outline");
   });
 });
