e1bc37f77d67c0ea358e21b348f28b77a744a721
David Puentes <pipe01112009@gmail.com>
2026-04-23 19:14:26 -0500

    Finishing lesson #247.

--- src/heroes/pages/search/ui/SearchControls.test.tsx
+++ src/heroes/pages/search/ui/SearchControls.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest";
 import { MemoryRouter } from "react-router";
 
 import SearchControls from "./SearchControls";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 const renderSearchControlsWithRouter = function (
   initialEntries: string[] = ["/"],
@@ -19,5 +19,30 @@ describe("SearchControls", () => {
     const { container } = renderSearchControlsWithRouter();
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('must set search input text as query parameter "search_query" is.', () => {
+    renderSearchControlsWithRouter(["/search/?search_query=Steve%20Rogers"]);
+
+    const inputElement = screen.getByPlaceholderText(
+      "Busca héroes, villanos, poderes, equipos, etc.",
+    );
+
+    expect(inputElement.getAttribute("value")).toBe("Steve Rogers");
+  });
+
+  test('must change query parameter "search_query" when input text was changed and enter key was pressed on it.', () => {
+    renderSearchControlsWithRouter(["/search/?search_query=Steve%20Rogers"]);
+
+    const inputElement = screen.getByPlaceholderText(
+      "Busca héroes, villanos, poderes, equipos, etc.",
+    );
+
+    expect(inputElement.getAttribute("value")).toBe("Steve Rogers");
+
+    fireEvent.change(inputElement, { target: { value: "Clark Kent" } });
+    fireEvent.keyDown(inputElement, { key: "Enter" });
+
+    expect(inputElement.getAttribute("value")).toBe("Clark Kent");
   });
 });
